237b9f6af7aa32f06ced15839026bf9033e5349e
sajbon <siboniecjr@gmail.com>
2024-03-26 04:43:24 +0100

    Reduced code in excel_function file, better way to solve the problem of cutting table was figured

--- excel_function.py
+++ excel_function.py
@@ -5,37 +5,30 @@ df = pd.read_excel("smieci.xlsx")
 
 
 def main():
-    day_win, month_win, year_win, hour_win = (
-        date_time()
-    )  # Day, Month, Year, Hour, acquired from windows system
-    month_year = (
-        month_win + " " + year_win
-    )  # Joined Month and Year (database has exact same type of written values)
+    # Day, Month, Year, Hour, acquired from windows system
+    day_win, month_win, year_win, hour_win = date_time()
 
-    i = 0  # added i variable to get index of a month in excel table
+    # Joined Month and Year (database has exact same type of written values)
+    month_year = month_win + " " + year_win
 
-    for (
-        column,
-        items,
-    ) in df.items():  # Iteration through values to find column "MIESIĄC"
-        if column == "MIESIĄC":
-            for (
-                month
-            ) in (
-                items.str.lower()
-            ):  # Iteration through values in "MIESIĄC to find value of the month (January, February and etc.)"
-                i = i + 1
-                if month == month_year:  # Finding current month
-                    present_month = month
-                    value = df.loc[[i - 1]]
-                    print(value)
+    message = "\n"
 
-                    # DZIEN MIESIAC ROK - TYP CZYLI NP ZMIESZANE
-                    # POTNIJ TO TAK
-        # else:
-        #     if column == present_month:
-        #         for schedule_date in df.iloc[:, 1:]:
-        #             print(schedule_date)
+    # Iterate over each row in the DataFrame
+    for index, row in df.iterrows():
+
+        # Checked if the month matches the current month
+        if row["MIESIĄC"].lower() == month_year.lower():
+
+            # Iterate over each column in the row
+            for column in df.columns[1:]:  # Skip the first column which is 'MIESIĄC'
+                if "," in str(row[column]):
+                    multiple_data = row[column].split(",")
+                    for single_data in multiple_data:
+                        message = str(single_data) + " " + month_year + " " + column
+                        print(message)
+                else:
+                    message = str(row[column]) + " " + month_year + " " + column
+                    print(message)
 
 
 if __name__ == "__main__":
